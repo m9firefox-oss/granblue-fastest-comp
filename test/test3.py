0e55d47996c92b36a0adf28b88238d7b9d76ab43
m9firefox-oss <m9firefox@gmail.com>
2026-06-28 18:32:35 +0900

    1

---
+++ test/test3.py
@@ -0,0 +1,13 @@
+﻿class MakeClass:
+    def __init__(self, id, name, price, purchase_price):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.purchase_price = purchase_price
+
+    def calculate_profit(self):
+        profit = self.price - self.purchase_price
+        return profit
+
+cool_tshirt = MakeClass("A0001", "Cool T-shirt", 5000, 2250)
+print(cool_tshirt.calculate_profit())
